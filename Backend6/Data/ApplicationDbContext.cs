7b8c3183e638a6372ec3c2729bda86c15dc6538d
Mr_GreZevS <danil_grezev@mail.ru>
2019-06-26 22:22:20 +0700

    Start project

---
+++ Backend6/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Backend6.Models;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Backend6.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+       
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+           
+        }
+    }
+}
