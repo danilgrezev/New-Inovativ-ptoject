0c160f46f7f839c1414c4f523d3f7842e52cc2d0
Unknown <ivandanilovich99@gmail.com>
2019-06-28 15:11:13 +0700

    save point
    delete migrations, controllers, views

--- Backend6/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Backend6/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -4,8 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.EntityFrameworkCore.Storage;
-using Microsoft.EntityFrameworkCore.Storage.Internal;
 using System;
 
 namespace Backend6.Data.Migrations
@@ -17,13 +15,12 @@ namespace Backend6.Data.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.0.3-rtm-10026")
+                .HasAnnotation("ProductVersion", "1.0.0-rc3")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
             modelBuilder.Entity("Backend6.Models.ApplicationUser", b =>
                 {
-                    b.Property<string>("Id")
-                        .ValueGeneratedOnAdd();
+                    b.Property<string>("Id");
 
                     b.Property<int>("AccessFailedCount");
 
@@ -34,8 +31,6 @@ namespace Backend6.Data.Migrations
                         .HasMaxLength(256);
 
                     b.Property<bool>("EmailConfirmed");
-
-                    b.Property<int>("GeoId");
 
                     b.Property<bool>("LockoutEnabled");
 
@@ -53,212 +48,28 @@ namespace Backend6.Data.Migrations
 
                     b.Property<bool>("PhoneNumberConfirmed");
 
-                    b.Property<int>("Rating");
-
                     b.Property<string>("SecurityStamp");
-
-                    b.Property<string>("Status");
 
                     b.Property<bool>("TwoFactorEnabled");
 
                     b.Property<string>("UserName")
                         .HasMaxLength(256);
 
-                    b.Property<int>("UserTypeId");
-
                     b.HasKey("Id");
-
-                    b.HasIndex("GeoId");
 
                     b.HasIndex("NormalizedEmail")
                         .HasName("EmailIndex");
 
                     b.HasIndex("NormalizedUserName")
                         .IsUnique()
-                        .HasName("UserNameIndex")
-                        .HasFilter("[NormalizedUserName] IS NOT NULL");
-
-                    b.HasIndex("UserTypeId");
+                        .HasName("UserNameIndex");
 
                     b.ToTable("AspNetUsers");
                 });
 
-            modelBuilder.Entity("DoNothing.Models.ApplicationUserTaskType", b =>
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityRole", b =>
                 {
-                    b.Property<int>("ApplicationUserId");
-
-                    b.Property<int>("TaskTypeId");
-
-                    b.Property<string>("ApplicationUserId1");
-
-                    b.HasKey("ApplicationUserId", "TaskTypeId");
-
-                    b.HasIndex("ApplicationUserId1");
-
-                    b.HasIndex("TaskTypeId");
-
-                    b.ToTable("ApplicationUserTaskTypes");
-                });
-
-            modelBuilder.Entity("DoNothing.Models.Card", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd();
-
-                    b.Property<int>("ApplicationUserId");
-
-                    b.Property<string>("ApplicationUserId1");
-
-                    b.Property<string>("CVV")
-                        .IsRequired();
-
-                    b.Property<string>("Code")
-                        .IsRequired();
-
-                    b.Property<string>("Date")
-                        .IsRequired();
-
-                    b.Property<string>("Name")
-                        .IsRequired();
-
-                    b.HasKey("Id");
-
-                    b.HasIndex("ApplicationUserId1");
-
-                    b.ToTable("Cards");
-                });
-
-            modelBuilder.Entity("DoNothing.Models.Comment", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd();
-
-                    b.Property<DateTime>("DateTime");
-
-                    b.Property<int>("RecipientId");
-
-                    b.Property<string>("RecipientId1");
-
-                    b.Property<int>("SenderId");
-
-                    b.Property<string>("SenderId1");
-
-                    b.Property<string>("Text");
-
-                    b.HasKey("Id");
-
-                    b.HasIndex("RecipientId1");
-
-                    b.HasIndex("SenderId1");
-
-                    b.ToTable("Comments");
-                });
-
-            modelBuilder.Entity("DoNothing.Models.Geo", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd();
-
-                    b.Property<string>("City");
-
-                    b.Property<string>("Country");
-
-                    b.Property<string>("District");
-
-                    b.HasKey("Id");
-
-                    b.ToTable("Geos");
-                });
-
-            modelBuilder.Entity("DoNothing.Models.Insurance", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd();
-
-                    b.Property<string>("Code")
-                        .IsRequired();
-
-                    b.Property<decimal>("Price");
-
-                    b.HasKey("Id");
-
-                    b.ToTable("Insurances");
-                });
-
-            modelBuilder.Entity("DoNothing.Models.MyTask", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd();
-
-                    b.Property<DateTime>("ApplyingTime");
-
-                    b.Property<int>("ClientId");
-
-                    b.Property<string>("ClientId1");
-
-                    b.Property<int>("EmployeeId");
-
-                    b.Property<string>("EmployeeId1");
-
-                    b.Property<DateTime>("ExecutionTime");
-
-                    b.Property<int>("GeoId");
-
-                    b.Property<string>("Header");
-
-                    b.Property<int>("InsuranceId");
-
-                    b.Property<int>("Priority");
-
-                    b.Property<string>("Status");
-
-                    b.Property<int>("TaskTypeId");
-
-                    b.Property<string>("Text");
-
-                    b.HasKey("Id");
-
-                    b.HasIndex("ClientId1");
-
-                    b.HasIndex("EmployeeId1");
-
-                    b.HasIndex("GeoId");
-
-                    b.HasIndex("InsuranceId");
-
-                    b.HasIndex("TaskTypeId");
-
-                    b.ToTable("Task");
-                });
-
-            modelBuilder.Entity("DoNothing.Models.TaskType", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd();
-
-                    b.Property<string>("Type");
-
-                    b.HasKey("Id");
-
-                    b.ToTable("TaskTypes");
-                });
-
-            modelBuilder.Entity("DoNothing.Models.UserType", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd();
-
-                    b.Property<string>("Type");
-
-                    b.HasKey("Id");
-
-                    b.ToTable("UserTypes");
-                });
-
-            modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRole", b =>
-                {
-                    b.Property<string>("Id")
-                        .ValueGeneratedOnAdd();
+                    b.Property<string>("Id");
 
                     b.Property<string>("ConcurrencyStamp")
                         .IsConcurrencyToken();
@@ -272,14 +83,12 @@ namespace Backend6.Data.Migrations
                     b.HasKey("Id");
 
                     b.HasIndex("NormalizedName")
-                        .IsUnique()
-                        .HasName("RoleNameIndex")
-                        .HasFilter("[NormalizedName] IS NOT NULL");
+                        .HasName("RoleNameIndex");
 
                     b.ToTable("AspNetRoles");
                 });
 
-            modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<string>", b =>
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityRoleClaim<string>", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd();
@@ -298,7 +107,7 @@ namespace Backend6.Data.Migrations
                     b.ToTable("AspNetRoleClaims");
                 });
 
-            modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityUserClaim<string>", b =>
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUserClaim<string>", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd();
@@ -317,7 +126,7 @@ namespace Backend6.Data.Migrations
                     b.ToTable("AspNetUserClaims");
                 });
 
-            modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityUserLogin<string>", b =>
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUserLogin<string>", b =>
                 {
                     b.Property<string>("LoginProvider");
 
@@ -335,7 +144,7 @@ namespace Backend6.Data.Migrations
                     b.ToTable("AspNetUserLogins");
                 });
 
-            modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityUserRole<string>", b =>
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUserRole<string>", b =>
                 {
                     b.Property<string>("UserId");
 
@@ -345,10 +154,12 @@ namespace Backend6.Data.Migrations
 
                     b.HasIndex("RoleId");
 
+                    b.HasIndex("UserId");
+
                     b.ToTable("AspNetUserRoles");
                 });
 
-            modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityUserToken<string>", b =>
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUserToken<string>", b =>
                 {
                     b.Property<string>("UserId");
 
@@ -363,116 +174,39 @@ namespace Backend6.Data.Migrations
                     b.ToTable("AspNetUserTokens");
                 });
 
-            modelBuilder.Entity("Backend6.Models.ApplicationUser", b =>
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityRoleClaim<string>", b =>
                 {
-                    b.HasOne("DoNothing.Models.Geo", "Geo")
-                        .WithMany("ApplicationUsers")
-                        .HasForeignKey("GeoId")
-                        .OnDelete(DeleteBehavior.Cascade);
-
-                    b.HasOne("DoNothing.Models.UserType", "UserType")
-                        .WithMany("ApplicationUsers")
-                        .HasForeignKey("UserTypeId")
-                        .OnDelete(DeleteBehavior.Cascade);
-                });
-
-            modelBuilder.Entity("DoNothing.Models.ApplicationUserTaskType", b =>
-                {
-                    b.HasOne("Backend6.Models.ApplicationUser", "ApplicationUser")
-                        .WithMany("TaskTypes")
-                        .HasForeignKey("ApplicationUserId1");
-
-                    b.HasOne("DoNothing.Models.TaskType", "TaskType")
-                        .WithMany("ApplicationUsers")
-                        .HasForeignKey("TaskTypeId")
-                        .OnDelete(DeleteBehavior.Cascade);
-                });
-
-            modelBuilder.Entity("DoNothing.Models.Card", b =>
-                {
-                    b.HasOne("Backend6.Models.ApplicationUser", "ApplicationUser")
-                        .WithMany("Cards")
-                        .HasForeignKey("ApplicationUserId1");
-                });
-
-            modelBuilder.Entity("DoNothing.Models.Comment", b =>
-                {
-                    b.HasOne("Backend6.Models.ApplicationUser", "Recipient")
-                        .WithMany()
-                        .HasForeignKey("RecipientId1");
-
-                    b.HasOne("Backend6.Models.ApplicationUser", "Sender")
-                        .WithMany()
-                        .HasForeignKey("SenderId1");
-                });
-
-            modelBuilder.Entity("DoNothing.Models.MyTask", b =>
-                {
-                    b.HasOne("Backend6.Models.ApplicationUser", "Client")
-                        .WithMany()
-                        .HasForeignKey("ClientId1");
-
-                    b.HasOne("Backend6.Models.ApplicationUser", "Employee")
-                        .WithMany()
-                        .HasForeignKey("EmployeeId1");
-
-                    b.HasOne("DoNothing.Models.Geo", "Geo")
-                        .WithMany("MyTasks")
-                        .HasForeignKey("GeoId")
-                        .OnDelete(DeleteBehavior.Cascade);
-
-                    b.HasOne("DoNothing.Models.Insurance", "Insurance")
-                        .WithMany()
-                        .HasForeignKey("InsuranceId")
-                        .OnDelete(DeleteBehavior.Cascade);
-
-                    b.HasOne("DoNothing.Models.TaskType", "TaskType")
-                        .WithMany()
-                        .HasForeignKey("TaskTypeId")
-                        .OnDelete(DeleteBehavior.Cascade);
-                });
-
-            modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<string>", b =>
-                {
-                    b.HasOne("Microsoft.AspNetCore.Identity.IdentityRole")
-                        .WithMany()
+                    b.HasOne("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityRole")
+                        .WithMany("Claims")
                         .HasForeignKey("RoleId")
                         .OnDelete(DeleteBehavior.Cascade);
                 });
 
-            modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityUserClaim<string>", b =>
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUserClaim<string>", b =>
                 {
                     b.HasOne("Backend6.Models.ApplicationUser")
-                        .WithMany()
+                        .WithMany("Claims")
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.Cascade);
                 });
 
-            modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityUserLogin<string>", b =>
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUserLogin<string>", b =>
                 {
                     b.HasOne("Backend6.Models.ApplicationUser")
-                        .WithMany()
+                        .WithMany("Logins")
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.Cascade);
                 });
 
-            modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityUserRole<string>", b =>
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUserRole<string>", b =>
                 {
-                    b.HasOne("Microsoft.AspNetCore.Identity.IdentityRole")
-                        .WithMany()
+                    b.HasOne("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityRole")
+                        .WithMany("Users")
                         .HasForeignKey("RoleId")
                         .OnDelete(DeleteBehavior.Cascade);
 
                     b.HasOne("Backend6.Models.ApplicationUser")
-                        .WithMany()
-                        .HasForeignKey("UserId")
-                        .OnDelete(DeleteBehavior.Cascade);
-                });
-
-            modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityUserToken<string>", b =>
-                {
-                    b.HasOne("Backend6.Models.ApplicationUser")
-                        .WithMany()
+                        .WithMany("Roles")
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.Cascade);
                 });
