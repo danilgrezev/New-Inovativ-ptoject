2188761e215081220ad535a64120fc352474f0c4
Viktor <anselm_v@mail.ru>
2019-06-30 00:13:29 +0700

    Добавлен мокап домашней страницы.

---
+++ Backend5/Controllers/MockupsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Backend5.Data;
+using Backend5.Models;
+
+namespace Backend5.Controllers
+{
+    public class MockupsController : Controller
+    {
+
+        public async Task<IActionResult> Home()
+        {
+
+            return View();
+        }
+    }
+
+}
