52277855d533b65fea497951dba5ee3d7bbe1c28
Unknown <ivandanilovich99@gmail.com>
2019-06-30 13:59:32 +0700

    save

--- Backend5/Startup.cs
+++ Backend5/Startup.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Backend5.Data;
+using Backend5.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,6 +34,47 @@ namespace Backend5
 
         }
 
+        private async Task ConfigureIdentity(IServiceScope scope)
+        {
+            var roleManager = scope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
+            var userManager = scope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
+
+            var adminsRole = await roleManager.FindByNameAsync(ApplicationRoles.Administrators);
+            if (adminsRole == null)
+            {
+                var roleResult = await roleManager.CreateAsync(new IdentityRole(ApplicationRoles.Administrators));
+                if (!roleResult.Succeeded)
+                {
+                    throw new InvalidOperationException($"Unable to create {ApplicationRoles.Administrators} role.");
+                }
+
+                adminsRole = await roleManager.FindByNameAsync(ApplicationRoles.Administrators);
+            }
+
+            var adminUser = await userManager.FindByNameAsync("admin@localhost.local");
+            if (adminUser == null)
+            {
+                var userResult = await userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "admin@localhost.local",
+                    Email = "admin@localhost.local"
+                }, "AdminPass123!");
+                if (!userResult.Succeeded)
+                {
+                    throw new InvalidOperationException($"Unable to create admin@localhost.local user");
+                }
+
+                adminUser = await userManager.FindByNameAsync("admin@localhost.local");
+            }
+
+            if (!await userManager.IsInRoleAsync(adminUser, adminsRole.Name))
+            {
+                await userManager.AddToRoleAsync(adminUser, adminsRole.Name);
+            }
+        }
+
+
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
@@ -39,7 +82,56 @@ namespace Backend5
             using (var context = scope.ServiceProvider.GetService<ApplicationDbContext>())
             {
                 context.Database.Migrate();
+                //  this.ConfigureIdentity(scope).GetAwaiter().GetResult();
+
+
+
+
+
+                var roleManager = scope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
+                var userManager = scope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
+
+                var adminsRole = await roleManager.FindByNameAsync(ApplicationRoles.Administrators);
+                if (adminsRole == null)
+                {
+                    var roleResult = await roleManager.CreateAsync(new IdentityRole(ApplicationRoles.Administrators));
+                    if (!roleResult.Succeeded)
+                    {
+                        throw new InvalidOperationException($"Unable to create {ApplicationRoles.Administrators} role.");
+                    }
+
+                    adminsRole = await roleManager.FindByNameAsync(ApplicationRoles.Administrators);
+                }
+
+                var adminUser = await userManager.FindByNameAsync("admin@localhost.local");
+                if (adminUser == null)
+                {
+                    var userResult = await userManager.CreateAsync(new ApplicationUser
+                    {
+                        UserName = "admin@localhost.local",
+                        Email = "admin@localhost.local"
+                    }, "AdminPass123!");
+                    if (!userResult.Succeeded)
+                    {
+                        throw new InvalidOperationException($"Unable to create admin@localhost.local user");
+                    }
+
+                    adminUser = await userManager.FindByNameAsync("admin@localhost.local");
+                }
+
+                if (!await userManager.IsInRoleAsync(adminUser, adminsRole.Name))
+                {
+                    await userManager.AddToRoleAsync(adminUser, adminsRole.Name);
+                }
+
+
+
+
+
+
+
             }
+
 
             if (env.IsDevelopment())
             {
